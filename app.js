bf49908ee16b57fbeef74853d27207d4c141baa3
Heather Swinn <h.swinn@Heathers-MacBook-Air.local>
2023-11-13 12:04:35 -0600

    feature 1 updated

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 function play() {
-    let secretNumber = 5;
+    let secretNumber = "5";
+    // let secretNumber = 5;
 
     while(true) {
-        const guess = prompt("Guess a number.");
-        if(guess === null){
-            alert("Goodbye!");
+        const guess = prompt("Guess a number!");
+        if (guess === null) {
+            alert("Goodbye!")
             break;
-        }if (guess == secretNumber){
-            alert("You are RIGHT!")
+        }
+        if (guess == secretNumber) {
+            alert("You win!")
             break;
+        }
+        if (guess > secretNumber) {
+            alert("Guess lower!");
+        }
+        if (guess < secretNumber) {
+            alert("Guess higher!");
         }
     }
 }
-play();
+
+play()
