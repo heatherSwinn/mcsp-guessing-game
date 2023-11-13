2fedd1f557ff70a8474d0b4cb10f9c4054294b39
Heather Swinn <h.swinn@Heathers-MacBook-Air.local>
2023-11-13 11:47:38 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function play() {
+    let secretNumber = 5;
+
+    while(true) {
+        const guess = prompt("Guess a number.");
+        if(guess == null){
+            alert("Goodbye!");
+            break;
+        }if (guess == secretNumber){
+            alert("You are RIGHT!")
+            break;
+        }
+    }
+}
+play();
