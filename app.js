0c6e8d5c72cc315e8a41363ec50774fc38292129
Heather Swinn <h.swinn@Heathers-MacBook-Air.local>
2023-11-13 13:43:19 -0600

    feature 5

--- app.js
+++ app.js
@@ -3,25 +3,33 @@ function play() {
     let attempts = 1;
     let tries = [];
 
+    const name = prompt("What is your name?");
 
     while(true) {
+
         const guess = prompt("Guess a number!");
         if (guess === null) {
             alert("Goodbye!")
             break;
         }
+        if (guess == secretNumber && attempts == 1) {
+            alert(`Congratulations ${name}! You won!`)
+            alert(`${name} you guessed right on the first try! The correct answer was ${secretNumber}.`)
+            break;
+        }
+
         if (guess == secretNumber) {
-            alert("You win!")
+            alert(`Congratulations ${name}! You won!`)
             alert(`It took you ${attempts} attempts! Your previous answers were ${tries}. The correct answer was ${secretNumber}.`)
             break;
         }
         if (guess > secretNumber) {
-            alert("Lower! Try again");
+            alert(`Sorry ${name}, guess lower!`);
 
 
         }
         if (guess < secretNumber) {
-            alert("Higher! Try again");
+            alert(`Sorry ${name}, guess higher!`);
 
         }
         tries.push(guess);
